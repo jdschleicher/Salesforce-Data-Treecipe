5d6228a171668a3dbc14399dc4fb1c2ca29a3187
jdschleicher <jdschleicher@gmail.com>
2024-11-21 22:43:00 -0500

    timestamp ...

--- src/treecipe/src/RecipeService/RecipeService.ts
+++ src/treecipe/src/RecipeService/RecipeService.ts
@@ -217,7 +217,15 @@ ${this.generateTabs(1)}${fieldPropertAndRecipeValue}`;
           vscode.window.showInformationMessage('Directory processing completed');
         }
       
-        const outputFilePath = `${workspaceRoot}/ouptut.yaml`;
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+        const day = String(now.getDate()).padStart(2, '0');
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const timestamp = `${year}${month}${day}_${hours}${minutes}`;
+        const recipeFileName = `recipe-${timestamp}.yaml`;
+        const outputFilePath = `${workspaceRoot}/treecipe/${recipeFileName}`;
         fs.writeFile(outputFilePath, objectsInfoWrapper.combinedRecipes, (err) => {
             if (err) {
                 console.error('Error writing file', err);
