3afd9bcc81f4ac00f093d3445cfa8e98abab0a19
jdschleicher <jdschleicher@gmail.com>
2024-11-21 22:42:54 -0500

    removing hello world command

--- src/extension.ts
+++ src/extension.ts
@@ -8,31 +8,17 @@ import { RecipeService } from './treecipe/src/RecipeService/RecipeService';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hrg" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('hrg.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from helloRecipeGenerator!');
-	});
-
-	const initiateConfiguration = vscode.commands.registerCommand('hrg.initiateConfiguration', () => {
+	const initiateConfiguration = vscode.commands.registerCommand('treecipe.initiateConfiguration', () => {
 		ConfigurationService.createConfigurationFile();
 	});
 
-	const generateTreecipe = vscode.commands.registerCommand('hrg.generateTreecipe', () => {
+	const generateTreecipe = vscode.commands.registerCommand('treecipe.generateTreecipe', () => {
 		
 		RecipeService.generateRecipeFromConfigurationDetail();
 		
 	});
 
 	context.subscriptions.push(
-		disposable, 
 		generateTreecipe,
 		initiateConfiguration
 	);
