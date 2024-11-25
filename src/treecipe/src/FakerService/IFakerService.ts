d5996066b86b816a02bccb7bc06a0e08bf13a10a
jdschleicher <jdschleicher@gmail.com>
2024-11-25 13:29:10 -0500

    beginnings of npm faker service

---
+++ src/treecipe/src/FakerService/IFakerService.ts
@@ -0,0 +1,7 @@
+
+export interface IFakerService {
+
+    getMapSalesforceFieldToFakerValue(): Record<string, string>;
+    // getFakeMultiSelectPicklistRecipeValueByXMLFieldDetail(commaJoinedPicklistChoices:string) : string;
+    // getFakePicklistRecipeValueByXMLFieldDetail()
+}
