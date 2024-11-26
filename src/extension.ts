e3b7faa37c24de2883d38c74b9f2e2ff5179460c
jdschleicher <jdschleicher@gmail.com>
2024-11-26 13:14:12 -0500

    adjustment to generatetreecipe command to find faker service

--- src/extension.ts
+++ src/extension.ts
@@ -3,18 +3,23 @@
 import * as vscode from 'vscode';
 import { ConfigurationService } from './treecipe/src/ConfigurationService/ConfigurationService';
 import { RecipeService } from './treecipe/src/RecipeService/RecipeService';
+import { IFakerService } from './treecipe/src/FakerService/IFakerService';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	const initiateConfiguration = vscode.commands.registerCommand('treecipe.initiateConfiguration', () => {
 		ConfigurationService.createConfigurationFile();
 	});
 
+
 	const generateTreecipe = vscode.commands.registerCommand('treecipe.generateTreecipe', () => {
 		
-		RecipeService.generateRecipeFromConfigurationDetail();
+		const configurationDetail = ConfigurationService.getConfigurationDetail();
+		let fakerServiceImplementation:IFakerService = ConfigurationService.getFakerImplementationByConfigurationSelelction(configurationDetail.dataFakerService);
+		let recipeService = new RecipeService(fakerServiceImplementation);
+		recipeService.generateRecipeFromConfigurationDetail();
 		
 	});
 
