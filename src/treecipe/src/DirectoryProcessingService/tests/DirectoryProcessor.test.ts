bc78e80cb77caecaebf797ca40d0305590fceaf2
jdschleicher <jdschleicher@gmail.com>
2024-12-19 15:33:50 -0500

    clean up and spacing

--- src/treecipe/src/DirectoryProcessingService/tests/DirectoryProcessor.test.ts
+++ src/treecipe/src/DirectoryProcessingService/tests/DirectoryProcessor.test.ts
@@ -18,20 +18,21 @@ jest.mock('vscode', () => ({
 
 }), { virtual: true });
 
-describe('Shared DirectoryProcessor Testign Context', () => {
 
-    // let directoryProcessor = new DirectoryProcessor();
+describe('Shared DirectoryProcessor Testign Context', () => {
     
     describe('given expected didddrectory of mock objects, returns expected folder structure', () => {
-    
+  
       test('given expected directory path segments, returns expected api name at end of path', () => {
 
         jest.spyOn(ConfigurationService, 'getExtensionConfigValue').mockReturnValue('snowfakery');
         
         const expectedObjectApiName = 'objectApiName';
-        let directoryProcessor = new DirectoryProcessor();
         let mockObjectsDirectoryPath = `src/treecipe/src/DirectoryProcessingService/tests/MockObjectsDirectory/objects/${expectedObjectApiName}`;   
+        
+        let directoryProcessor = new DirectoryProcessor();
         let actualLastPathSegmentValue = directoryProcessor.getLastSegmentFromPath(mockObjectsDirectoryPath);
+        
         expect(actualLastPathSegmentValue).toEqual(expectedObjectApiName);
       
       });
