3c828ca8f2731adf3d9256480ea84e601a9ca151
jdschleicher <jdschleicher@gmail.com>
2025-01-03 13:47:24 -0500

    test files

---
+++ snowfakery.ps1
@@ -0,0 +1,8 @@
+
+function
+
+
+snowfakery treecipe/GeneratedRecipes/recipe-2025-01-03T15-45-06.yaml    --output-format json --output-file 'thisname.json' --output-folder .
+
+
+snowfakery treecipe/GeneratedRecipes/recipe-2025-01-03T15-45-06.yaml    --output-format json --output-file 'thisname.csv' --output-folder .
