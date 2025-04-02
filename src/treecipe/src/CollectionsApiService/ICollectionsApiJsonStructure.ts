22cf2bf966442fcf0f8b8a05dbf8e6a691c53728
jdschleicher <jdschleicher@gmail.com>
2025-04-02 07:16:01 -0400

    undoing comment

--- src/treecipe/src/CollectionsApiService/ICollectionsApiJsonStructure.ts
+++ src/treecipe/src/CollectionsApiService/ICollectionsApiJsonStructure.ts
@@ -1,8 +1,3 @@
-// interface CollectionsApiStructure {
-//     allOrNone: boolean;
-//     records: any[];
-// }
-
 interface CollectionsApiJsonStructure {
     allOrNone: boolean;
     records: any[];
