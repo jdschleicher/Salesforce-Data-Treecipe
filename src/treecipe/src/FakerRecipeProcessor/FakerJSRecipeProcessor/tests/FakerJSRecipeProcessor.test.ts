cc634e46f9ae702c1c3144ecac75941ff7604c4b
jdschleicher <jdschleicher@gmail.com>
2025-04-28 23:39:13 -0400

    replacing faker-js require statement with import

--- src/treecipe/src/FakerRecipeProcessor/FakerJSRecipeProcessor/tests/FakerJSRecipeProcessor.test.ts
+++ src/treecipe/src/FakerRecipeProcessor/FakerJSRecipeProcessor/tests/FakerJSRecipeProcessor.test.ts
@@ -7,7 +7,7 @@ import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 
 
-const { faker } = require('@faker-js/faker');
+import { faker } from '@faker-js/faker';
 
 // jest.mock('@faker-js/faker', () => ({
 
